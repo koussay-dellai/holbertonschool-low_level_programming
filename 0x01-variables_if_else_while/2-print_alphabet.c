63ed86187a1fd7b2ccd9a543d47b6118d8fa65f4
koussay-dellai <koussaydellai22@gmail.com>
2019-09-19 14:18:59 +0100

    the low lev prog

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,15 @@
-#include <stdlib.h>
 #include <stdio.h>
- /**
- * main - Entry block
- * @void: no argument
- * Return: 0 (Sucess)
-**/
+/**
+ *main-print alphabet
+ *Description: printing alphabet
+ *from a to z
+ *Return: 0
+ */
 int main(void)
 {
-int i;
-char letters[26] = {
-'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-for (i = 0; i < 26; i++)
-{
-putchar (letters[i]);
-}
+int x;
+for (x = 'a'; x <= 'z'; x++)
+putchar (x);
+putchar('\n');
 return (0);
 }
