55a28cf62b6abf05e9c8fd753a5e797298e6bd83
koussay-dellai <koussaydellai22@gmail.com>
2019-09-19 15:10:41 +0100

    the low lev

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 /**
- *main-print alphabet
- *Description: printing alphabet lowercase and uppercase
- *from a to z and from A to Z
- *Return: 0
+ *main - print alphabet
+ *Description: use putchar to print alphabet in
+ *lowercase
+ *Return: 0 is true
  */
 int main(void)
 {
-int char1, char2;
- for (char1 = 'A'; char1 <= 'Z'; char1++);
-
-putchar(char1);
-
- for (char2 = 'a'; char2 <= 'z'; char2++);
-
-putchar(char2);
-
+int x;
+for (x = 'a'; x <= 'z'; x++)
+putchar(x);
+for (x = 'A'; x <= 'Z'; x++)
+putchar (x);
 putchar('\n');
 return (0);
 }
