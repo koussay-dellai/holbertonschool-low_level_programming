bfb8b3d6a2e3332ee3c53607779c54838ef9b0fd
koussay-dellai <koussaydellai22@gmail.com>
2019-09-26 12:53:17 +0100

    the nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * mul - check the code for Holberton School students.
+ * @a : variable.
+ * @b : another variable .
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
