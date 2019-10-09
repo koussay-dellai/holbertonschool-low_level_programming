5756be9e0062e3ebabc48c4b45e80fea855b2148
koussay-dellai <koussaydellai22@gmail.com>
2019-10-09 14:48:13 +0100

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *_puts_recursion - recursion function that prints a string.
+ *@s: pointer to string .
+ *description: print a string and new line without a loop.
+ *return: always success.
+ */
+
+void _puts_recursion(char *s)
+{
+  if ( *s == '\0')
+    {
+      _putchar('\n');
+      return;
+    }
+  _putchar(*s);
+  _puts_recursion(s+1);
+}
