a465cc01f056dade201aac9527b02c4a1780ceb1
koussay-dellai <koussaydellai22@gmail.com>
2019-10-16 14:49:29 +0100

    malloc and free

---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,8 @@
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+int _strlen(char *s);
+char *_strncat(char *dest, char *src, int n);
+char *str_concat(char *s1, char *s2);
+void free_grid(int **grid, int height);
+int **alloc_grid(int width, int height);
