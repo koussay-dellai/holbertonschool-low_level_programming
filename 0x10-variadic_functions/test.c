de092bf12b764dd285872cdf2c06b20c7f23e15f
koussay-dellai <koussaydellai22@gmail.com>
2019-10-27 23:46:45 +0100

    the variadic functions

---
+++ 0x10-variadic_functions/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ *print_all - printing
+ *@format: pointer to typedef
+ *Return: nothing
+ */
+
+void print_all(const char * const format, ...)
+{
