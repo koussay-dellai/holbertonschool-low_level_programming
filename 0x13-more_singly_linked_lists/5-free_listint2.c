a8d9cfe0966e35c615c922bc431c88f09b0c68cf
koussay-dellai <koussaydellai22@gmail.com>
2019-11-06 03:18:26 +0100

    deleting linked list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,12 +6,13 @@
  */
 void free_listint2(listint_t **head)
 {
-listint_t *current_node = *head;
+listint_t *current_node = NULL;
 listint_t *next_node = NULL;
-if (*head == NULL)
+if (head == NULL)
 {
 return;
 }
+current_node = *head;
 while (current_node != NULL)
 {
 next_node = current_node->next;
