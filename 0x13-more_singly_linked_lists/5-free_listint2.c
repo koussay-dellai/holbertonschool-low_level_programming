e42fa6372efa4f323f371cca052ae59565ebe52e
koussay-dellai <koussaydellai22@gmail.com>
2019-11-06 03:13:55 +0100

    deleting all the node and setting the head to null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,6 +8,10 @@ void free_listint2(listint_t **head)
 {
 listint_t *current_node = *head;
 listint_t *next_node = NULL;
+if (*head == NULL)
+{
+return;
+}
 while (current_node != NULL)
 {
 next_node = current_node->next;
