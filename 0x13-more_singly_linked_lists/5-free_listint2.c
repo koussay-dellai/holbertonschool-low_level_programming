48e49fbf70a131e5181bedee3fa13bd19a632865
koussay-dellai <koussaydellai22@gmail.com>
2019-11-06 03:08:26 +0100

    deleting the nodes of a linked list and derefrence the head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ *free_listint2 - deleting all the nodes of a linked list
+ *@head: pointer to pointer to the first element of the node
+ *Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *current_node = *head;
+listint_t *next_node = NULL;
+while (current_node != NULL)
+{
+next_node = current_node->next;
+free(current_node);
+current_node = next_node;
+}
+*head = NULL;
+}
