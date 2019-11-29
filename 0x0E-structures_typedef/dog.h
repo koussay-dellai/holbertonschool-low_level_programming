6b292215f6dd35215ee4f9f7c4298b7952faecc0
koussay-dellai <koussaydellai22@gmail.com>
2019-11-29 17:14:16 +0100

    printing the elements of struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
 /**
  *struct dog - structure
  *@name: pointer to string
@@ -15,6 +21,6 @@ char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
+void print_dog(struct dog *d);
 
 #endif
