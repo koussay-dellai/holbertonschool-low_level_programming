00460d6e33279542628786eaff414e7209b57ea8
koussay-dellai <koussaydellai22@gmail.com>
2019-11-29 16:53:36 +0100

    struct initialization

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ *init_dog - initialize a variable of type strcut dog
+ *@d: variable of type dog
+ *@name: pointer to a char
+ *@age: variable of type float
+ *@owner: pointer to a char
+ *Return: Nothinig
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d == NULL)
+  return;
+d->name = name;
+d->age = age;
+d->owner = owner;
+return;
+}
