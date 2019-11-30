5d99ec747c3dc23d5d5b54f8d3368e383497b4cc
koussay-dellai <koussaydellai22@gmail.com>
2019-11-30 01:31:03 +0100

    printiong the struct content

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,10 +13,12 @@ if (d == NULL)
 return;
 if (d->name == NULL)
 printf("Name: (nil)\n");
+else
 printf("Name: %s\n", d->name);
 printf("Age: %f\n", d->age);
 if (d->owner == NULL)
 printf("Owner: (nil)\n");
+else
 printf("Owner: %s\n", d->owner);
 return;
 }
